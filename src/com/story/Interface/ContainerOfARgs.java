6044b12b7d6be904c7d73fbb3d19d485118b4534
FooolyHARD <k.arsgoo@gmail.com>
2022-01-21 18:27:50 +0300

    ь

--- src/com/story/Interface/ContainerOfARgs.java
+++
@@ -1,5 +0,0 @@
-package com.story.Interface;
-
-public interface ContainerOfARgs {
-    String getName();
-}
--- src/com/story/Interface/NotALive.java
+++
@@ -1,6 +0,0 @@
-//NotALive.java
-package com.story.Interface;
-
-public interface NotALive {
-    void exist();
-}
